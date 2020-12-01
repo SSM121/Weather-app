f8cbff5cb2457f57652110b73757f1b11474bef2
Spencer Kasteler <sdkasteler@gmail.com>
2020-12-01 14:38:49 -0700

    got all the api calls working. also made the display for the ip info in vue form. still need to convert the 2 weather json objects into vue objects

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 const ipKey = "9711c8e66f346046c177f533fb24b3c8";
 const weatherKey = "41b6d592f9edc42581720bae05f2d6cf";
-var ipInfo;
+//var ipInfo;
 var currentWeather;
+var forcastWeather;
 var request;
+
+const app = Vue.createApp({
+	data(){
+		return{
+			ipReady: false,
+			ipInfo: null,
+		}
+	}
+});
+
+const vm = app.mount("#app");
+
+
 
 function init(){
 	fetch("http://api.ipstack.com/check?access_key=" + ipKey)
 		.then(response => response.json())
 		.then(data => {
-			ipInfo = data;
-			request = "http://api.openweathermap.org/data/2.5/weather?lat=" + ipInfo.latitude + "&lon=" + ipInfo.longitude + "&appid=" + weatherKey;
+			vm.ipInfo = data;
+			request = "http://api.openweathermap.org/data/2.5/weather?lat=" + vm.ipInfo.latitude + "&lon=" + vm.ipInfo.longitude + "&appid=" + weatherKey + "&units=imperial";
+			vm.ipReady = true;
 			return fetch(request);
 		})
 		.then(response =>response.json())
 		.then(json => {
 			currentWeather = json;
+			request = "http://api.openweathermap.org/data/2.5/forecast?lat=" + vm.ipInfo.latitude + "&lon=" + vm.ipInfo.longitude + "&appid=" + weatherKey + "&units=imperial";
+			return fetch(request);
+		})
+		.then(response => response.json())
+		.then(json => {
+			forcastWeather = json;
 		});
 }
+
+init();
