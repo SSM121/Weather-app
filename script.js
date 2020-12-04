490b7656a17f1046df515c79b3cad41fc43a4cb5
Spencer Kasteler <sdkasteler@gmail.com>
2020-12-03 17:38:25 -0700

    the project should be done but some doc

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const ipKey = "9711c8e66f346046c177f533fb24b3c8";
 const weatherKey = "41b6d592f9edc42581720bae05f2d6cf";
-//var ipInfo;
-var currentWeather;
-var forcastWeather;
+
 var request;
 
 const app = Vue.createApp({
@@ -10,13 +8,62 @@ const app = Vue.createApp({
 		return{
 			ipReady: false,
 			ipInfo: null,
+			currWeatherReady: false,
+			currWeather: null,
+			date: null,
+			forcastWeather: null,
+			forcastWeatherReady: false,
+			unlikely: 0,
+			neutral: 40,
+			likely: 0,
+			forcasts: [],
+		}
+	},
+	methods:{
+		activate(id, event){
+			var f = this.forcasts[id];
+			if(f.isNeutral){
+				f.isNeutral = false;
+				this.neutral--;
+				f.isLikely = true;
+				this.likely++;
+			} else if(f.isLikely){
+				f.isLikely = false;
+				this.likely--;
+				f.isUnlikely = true;
+				this.unlikely++;
+			} else if(f.isUnlikely){
+				f.isUnlikely = false;
+				this.unlikely--;
+				f.isNeutral = true;
+				this.neutral++
+			}
 		}
 	}
 });
 
 const vm = app.mount("#app");
 
-
+function initForcasts(){
+	var loc = vm.forcastWeather.list
+	for(i = 0; i < 40; i++){
+		var d = new Date(loc[i].dt_txt);
+		vm.forcasts[i] = {
+			id: i,
+			date: (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear() + " " + d.toLocaleTimeString('en-US'),
+			temp: loc[i].main.temp,
+			temp_max: loc[i].main.temp_max,
+			temp_min: loc[i].main.temp_min,
+			sky: loc[i].weather[0].description,
+			humidity: loc[i].main.humidity,
+			pressure: loc[i].main.pressure,
+			//0 = neutral, 1 = likely, -1 = unlikely
+			isNeutral: true,
+			isLikely: false,
+			isUnlikely: false,
+		}
+	}
+}
 
 function init(){
 	fetch("http://api.ipstack.com/check?access_key=" + ipKey)
@@ -29,13 +76,18 @@ function init(){
 		})
 		.then(response =>response.json())
 		.then(json => {
-			currentWeather = json;
+			vm.currWeather = json;
+			var d = new Date();
+			vm.date = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear() + " " + d.toLocaleTimeString('en-US');
+			vm.currWeatherReady = true;
 			request = "http://api.openweathermap.org/data/2.5/forecast?lat=" + vm.ipInfo.latitude + "&lon=" + vm.ipInfo.longitude + "&appid=" + weatherKey + "&units=imperial";
 			return fetch(request);
 		})
 		.then(response => response.json())
 		.then(json => {
-			forcastWeather = json;
+			vm.forcastWeather = json;
+			initForcasts();
+			vm.forcastWeatherReady = true;
 		});
 }
 
